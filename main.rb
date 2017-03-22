9dfa3fe721aeb76f0da60bc7f0a519bffa9f88f6
Travis Bigelow <tbigelow22@gmail.com>
2017-03-21 21:35:01 -0600

    more changes have been made. Now there's a db connection and DB

--- main.rb
+++ main.rb
@@ -1,9 +1,21 @@
 require 'sinatra'
 require 'slim'
 require 'shotgun'
+require 'data_mapper'
 
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Task
+  include DataMapper::Resource
+  property :id,           Serial
+  property :name,         String, :required => true
+  property :completed_at, DateTime
+end
+
+DataMapper.finalize
 
 get '/' do
+  @tasks = Task.all
   slim :index
 end
 
@@ -11,7 +23,6 @@ get '/:task' do
   @task = params[:task].split('-').join(' ').capitalize
   slim :task
 end
-
 
 post '/' do
   @task = params[:task]
